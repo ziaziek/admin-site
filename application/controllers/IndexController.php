169451d85c467e9943315631537cabb87beb3bae
przemo <przmnwck@gmail.com>
2012-12-31 21:06:15 +0100

    Added new file notatki.txt where I write down my notes and shortcuts. Users model created by using zf create db-table command.

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -6,6 +6,16 @@ class IndexController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
+        $users = new Application_Model_DbTable_Users();
+        if($users->fetchAll()->count()==0){
+            $this->view->numofusers='No users!';
+            
+            $users->insert(array('id'=>'1', 'username'=>'nikt', 'password'=>'password'));
+        } else {
+            
+            $u = $users->fetchAll()->current();
+            $this->view->u=$u->id;
+        }
     }
 
     public function indexAction()
